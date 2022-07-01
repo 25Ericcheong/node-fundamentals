fd7a87bb64027ecd4f7fbdcd1e84e6e7a5ebf40d
25Ericcheong <ericcheongkaikit@gmail.com>
2022-07-02 09:55:10 +1000

    started event module

--- app/app.js
+++ app/app.js
@@ -1,11 +1,5 @@
-const fs = require("fs");
+// naming convention used to indicate that this is a class
+// has tone of useful properties and methods
+const EventEmitter = require("events");
 
-// synchronously returns all files in current directory
-const files = fs.readdirSync("./");
-console.log(files);
-
-// asynchronously returns all files in current directory
-fs.readdir("./", function (err, files) {
-  if (err) console.log("Error", err);
-  else console.log("Result", files);
-});
+const emitter = new EventEmitter(); // this is an object
