23c1f6a5220f2c430f7fec81ad24a83dab5ec1ef
25Ericcheong <ericcheongkaikit@gmail.com>
2022-07-02 10:02:59 +1000

    emit example but wont emit

--- app/app.js
+++ app/app.js
@@ -3,3 +3,7 @@
 const EventEmitter = require("events");
 
 const emitter = new EventEmitter(); // this is an object
+
+// emit means make a noise or produce something (signaling that an event has occurred)
+// running script will not do anything because there are no listeners created to listen for this
+emitter.emit("messageLogged");
