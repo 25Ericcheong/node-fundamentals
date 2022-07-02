3c99d0cb5b7741486109bfcac69dbcb211de474a
25Ericcheong <ericcheongkaikit@gmail.com>
2022-07-02 11:07:08 +1000

    create custom class which extends event emitter and use inherited method

--- app/logger.js
+++ app/logger.js
@@ -3,12 +3,16 @@ const emitter = new EventEmitter(); // this is an object
 
 var url = "http://mylogger.io/log";
 
-function log(message) {
-  // sends http request
-  console.log(message);
+// logger class will now have all properties from EventEmitter
+class Logger extends EventEmitter {
+  // do not need function since this will now be a method of a class instead
+  log(message) {
+    // sends http request
+    console.log(message);
 
-  // raise an event
-  emitter.emit("messageLogged", { id: 1, url: "http://" });
+    // raise an event
+    this.emit("messageLogged", { id: 1, url: "http://" });
+  }
 }
 
-module.exports = log;
+module.exports = Logger;
