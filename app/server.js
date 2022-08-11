363bb3fd66164cb6fdd70c50c0c7bed45eb1cabe
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-11 21:30:07 +1000

    console module can also be used to measure performance of how long it took for a function to run

--- app/server.js
+++ app/server.js
@@ -25,3 +25,12 @@ console.countReset('orange');
 oranges.forEach(fruit => {
   console.count(fruit);
 });
+
+const doSomething = () => console.log('test');
+const measureDoingSomething = () => {
+  console.time('doSomething()');
+  // do something, and measure the time it takes
+  doSomething();
+  console.timeEnd('doSomething()');
+};
+measureDoingSomething();
