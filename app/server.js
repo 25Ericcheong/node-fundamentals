63a485098434d16c9cdea0fbd5760d406a86a79f
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-14 11:49:59 +1000

    simplified way of parsing with different syntax since node v10

--- app/server.js
+++ app/server.js
@@ -3,15 +3,30 @@
 //  todo: 'Buy the milk',
 // }
 
-const server = http.createServer((req, res) => {
-  let data = '';
+// const server = http.createServer((req, res) => {
+//   let data = '';
 
-  // assuming string is being passed here
-  req.on('data', chunk => {
-    data += chunk;
-  });
-  req.on('end', () => {
-    console.log(JSON.parse(data).todo); // 'Buy the milk'
-    res.end();
-  });
+//   // assuming string is being passed here
+//   req.on('data', chunk => {
+//     data += chunk;
+//   });
+//   req.on('end', () => {
+//     console.log(JSON.parse(data).todo); // 'Buy the milk'
+//     res.end();
+//   });
+// });
+
+// from Node.js v10 for await .. of syntax is available which simplifies the code above to the following
+
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const data = Buffer.concat(buffers).toString();
+
+  console.log(JSON.parse(data).todo); // 'Buy the milk'
+  res.end();
 });
