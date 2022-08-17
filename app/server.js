b178e59c0ae9db842c11f629a78c68b43095b581
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-17 22:27:02 +1000

    callback hell example and an example of using the async readfile and writefile method

--- app/server.js
+++ app/server.js
@@ -58,13 +58,3 @@
 
 // console.log(names)
 // console.log(data)
-
-const { readFile, writeFile } = require('fs')
-
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  console.log(result)
-})
