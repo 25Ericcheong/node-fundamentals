c15f6a40c912cc39fef0b7a07844538640688485
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-17 21:36:33 +1000

    adding flag a appends existing file with new content

--- app/server.js
+++ app/server.js
@@ -66,5 +66,6 @@ const second = readFileSync('./content/second.txt','utf-8')
 
 writeFileSync(
   './content/result-sync.txt',
-   `Here is the result : ${first} ${second}`
+   `Here is the result : ${first} ${second}`,
+   { flag: 'a' }
 )
