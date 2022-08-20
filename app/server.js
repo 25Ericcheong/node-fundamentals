eb9e8f4b1fc760f057e85a2fd9165065f19f958e
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-20 16:42:52 +1000

    included gitignore to exclude package moduels and additional notes on a utility package and using it in a simple example

--- app/server.js
+++ app/server.js
@@ -59,3 +59,9 @@
 // console.log(names)
 // console.log(data)
 
+// lodash is a utility library
+const _ = require('lodash')
+
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);
