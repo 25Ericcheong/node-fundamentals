343a2e24552575d7f98f737912470cc53d26d530
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-22 22:27:40 +1000

    example of blocking sync code

--- app/server.js
+++ app/server.js
@@ -63,6 +63,14 @@ if (req.url === '/') {
 }
 
 if (req.url === '/about') {
+
+  // blocking code
+  for (let i = 0; i < 1000; i++) {
+    for (let j = 0; j < 1000; j++) {
+      console.log(`${i} ${j}`)
+    }
+  }
+
   res.end('About page')
   return;
 }
