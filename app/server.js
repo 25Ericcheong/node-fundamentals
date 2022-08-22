74c3f7c05c5ace082524390994d7a432dccee0f2
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-22 22:16:31 +1000

    setup prior to including code blocking example

--- app/server.js
+++ app/server.js
@@ -53,3 +53,14 @@
 // example();
 
 // left the above for reference
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+
+
+  
+})
+
+server.listen(5000, () => {
+  console.log("Server listening on port 5000...")
+})
