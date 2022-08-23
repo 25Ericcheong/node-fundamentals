12762ea30d390f1d351f9a963991a990d87a6ce0
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-23 22:24:51 +1000

    moved code to dedicated file

--- app/server.js
+++ app/server.js
@@ -53,32 +53,4 @@
 // example();
 
 // left the above for reference
-const http = require("http")
 
-const server = http.createServer((req, res) => {
-
-if (req.url === '/') {
-  res.end('Home page')
-  return;
-}
-
-if (req.url === '/about') {
-
-  // blocking code
-  for (let i = 0; i < 1000; i++) {
-    for (let j = 0; j < 1000; j++) {
-      console.log(`${i} ${j}`)
-    }
-  }
-
-  res.end('About page')
-  return;
-}
-
-res.end('Error page')
-  
-})
-
-server.listen(5000, () => {
-  console.log("Server listening on port 5000...")
-})
