616bb6f37af07031913006b253b6697eba233a1f
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-24 22:33:52 +1000

    problem with current code is that if we want to do multiple actions, will result in callback hell

--- app/server.js
+++ app/server.js
@@ -54,3 +54,12 @@
 
 // left the above for reference
 
+const { readFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, data) => {
+  if (err) {
+    return
+  } else {
+    console.log(data)
+  }
+})
