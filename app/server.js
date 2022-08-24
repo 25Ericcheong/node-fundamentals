72d52c97969a1eb91a2ba498d8eee36ed2e492b1
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-24 22:42:40 +1000

    slightly more clean with promises but can still be complex with multiple actions still

--- app/server.js
+++ app/server.js
@@ -56,10 +56,20 @@
 
 const { readFile } = require('fs')
 
-readFile('./content/first.txt', 'utf8', (err, data) => {
-  if (err) {
-    return
-  } else {
-    console.log(data)
-  }
-})
+getText = (path) => {
+  return new Promise ((resolve, reject) => {
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.log('error')
+        reject(err)
+      } else {
+        console.log('success')
+        resolve(data)
+      }
+    })
+  })
+}
+
+getText('./content/first.txt')
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err))
