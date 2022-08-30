a666592a33c62802fed2ce207ed9a308f767b83f
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-30 16:02:16 +0800

    writing headers and content type of responds when sending to client

--- app/express-fundamentals/app.js
+++ app/express-fundamentals/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+// below will always be invoked when user tries to request for something from server via url
+const server = http.createServer((req, res) => {
+  res.writeHead(
+    200, 
+    {'content-type' : 'text/html'}
+  )
+  res.write('<h1>Home page</h1>')
+  res.end()
+})
+
+server.listen(5000)
