513105f647deb7b9abf3703b7e07afa523abb78f
25Ericcheong <ericcheongkaikit@gmail.com>
2022-08-30 16:32:54 +0800

    simple server capable of receiving different endpoints

--- app/express-fundamentals/app.js
+++ app/express-fundamentals/app.js
@@ -2,11 +2,31 @@ const http = require('http')
 
 // below will always be invoked when user tries to request for something from server via url
 const server = http.createServer((req, res) => {
-  res.writeHead(
-    200, 
-    {'content-type' : 'text/html'}
-  )
-  res.write('<h1>Home page</h1>')
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(
+      200, 
+      {'content-type' : 'text/html'}
+    )
+    res.write('<h1>Home page</h1>')
+  }
+
+  else if (url === '/about') {
+    res.writeHead(
+      200, 
+      {'content-type' : 'text/html'}
+    )
+    res.write('<h1>About page</h1>')
+  }
+
+  else {
+    res.writeHead(
+      404, 
+      {'content-type' : 'text/html'}
+    )
+    res.write('<h1>Page not found</h1>')
+  }
+  
   res.end()
 })
 
