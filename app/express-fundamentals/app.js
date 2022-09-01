7d8a03ee5dc04c97a9b0e54540c4796c5530e5f8
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-01 11:29:23 +0800

    alternate to sending index file to client

--- app/express-fundamentals/app.js
+++ app/express-fundamentals/app.js
@@ -7,11 +7,13 @@ const app = express()
 // setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
+// 2 ways of rendering index files to client
+// first is the below and the second is server side rendering (template engine) or just have index file in the public folder as well since by default index  file is sent by server anyway
+// app.get('/', (req, res) => {
 
-  // path.resolve would require absolute path
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+//   path.resolve would require absolute path
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (req, res) => {
   res.status(404).send('Page not found')
