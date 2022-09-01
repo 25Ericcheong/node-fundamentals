29aa40d1e23bc935d1b9f1b94221f636b51cbf0e
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-01 10:44:06 +0800

    simple home page responds with express

--- app/express-fundamentals/app.js
+++ app/express-fundamentals/app.js
@@ -3,3 +3,20 @@ const express = require('express')
 // alternative to the below line is also
 // app = require('express')() - imports module and also invokes it
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Home page')
+})
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000')
+})
+
+// most widely used methods
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
