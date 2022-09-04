d1cb4531b5ad268d9cb96174a7925ef6a076e1ad
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-04 21:16:37 +1000

    example of parsing incoming data to be used in the backend using in built express method

--- app/express-fundamentals/app.js
+++ app/express-fundamentals/app.js
@@ -5,11 +5,20 @@ let { people } = require('./data')
 // static assets
 app.use(express.static('./methods-public'))
 
+// parse form data 
+// parses incoming data from client
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({
     success: true,
     data: people
   })
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.send('Success')
 })
 
 app.listen(5000, () => {
