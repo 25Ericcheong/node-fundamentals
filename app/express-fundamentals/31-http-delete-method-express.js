0dcda360c67a30d7113f22acc20cc127a04f04e5
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-07 20:03:43 +1000

    isolate code for future reference

---
+++ app/express-fundamentals/31-http-delete-method-express.js
@@ -0,0 +1,94 @@
+const { application } = require('express')
+const express = require('express')
+const app = express()
+let { people } = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+
+// parse form data 
+// parses incoming data from client
+// note: app.use would apply all routes with this built-in express middleware
+app.use(express.urlencoded({ extended: false }))
+
+// parse json - for straight up http request (not form data)
+// additional middleware to parse
+app.use(express.json())
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: people
+  })
+})
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+
+  if (!name) {
+    return res.status(400).json(
+      {
+        success: false, 
+        msg: 'Please provide name'
+      })
+  }
+  
+  return res.status(201).json(
+    {
+      success: true, 
+      person: name
+    })
+})
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please provide credentials')
+})
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res.status(404).json
+    ({
+      success: false, 
+      msg: `No person with id ${id}` 
+    })
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+
+  res.status(200).json({success: true, data: newPeople})
+})
+
+app.delete('/api/people/:id', (req,res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+
+  if (!person) {
+    return res.status(404).json
+    ({
+      success: false, 
+      msg: `No person with id ${req.params.id}` 
+    })
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+  return res.status(200).json({ success: true, data: newPeople })
+})
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000')
+})
